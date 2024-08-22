748625b9f9539852cdc46f1ebb6dcf4e1c441ac2
Simon <besnard@gfz-potsdam.de>
2024-08-22 16:57:52 +0200

    update authentification

--- scripts/EarthData_authentification.py
+++ scripts/EarthData_authentification.py
@@ -2,5 +2,5 @@ from gedidb.downloader.authentication import EarthDataAuthenticator
 
 
 #%% Authentification to EarthData 
-authenticator = EarthDataAuthenticator(config_file='/home/simon/Documents/science/GFZ/projects/gedi-toolbox/config_files/earth_data_credentials.yml')
+authenticator = EarthDataAuthenticator(config_file='/home/simon/Documents/science/GFZ/projects/gedi-toolbox/config_files/data_config.yml')
 authenticator.authenticate()
