a10972be05441a6fe69bae6360b94bbed18ae2fe
Simon <besnard@gfz-potsdam.de>
2024-09-03 12:34:31 +0200

    finish up data_query module

--- scripts/database_query.py
+++ scripts/database_query.py
@@ -5,22 +5,28 @@ Created on Mon Sep  2 17:08:23 2024
 
 @author: simon
 """
+import geopandas as gpd
+
 from gedidb.providers.db_query import SQLQueryBuilder, GediDatabase
 
-# Instantiate the database connection
+#%% Load region of interest
+roi_ = gpd.read_file("./data/geojson/test.geojson")
+
+#%% Instantiate the database connection
 config_file = './config_files/data_config.yml' 
 db = GediDatabase(data_config_file = config_file)
 
-# Create the query builder
+#%% Create the query builder
 query_builder = SQLQueryBuilder(
                                 table_name="filtered_l2ab_l4ac_shots",
-                                columns=["shot_number", "beam_name", "rh"],
-                                start_time="2023-01-01",
+                                columns=["shot_number", "beam_name", "pai", "pavd_z", 'geometry'],
+                                #geometry = roi_,
+                                start_time="2021-01-01",
                                 end_time="2023-12-31",
-                                limit=100,
+                                limit=1000,
                                 force=True,
                                 order_by=["-shot_number"]
                             )
 
-# Execute the query
+#%% Execute the query
 df = db.query("filtered_l2ab_l4ac_shots", query_builder=query_builder, use_geopandas=True)
