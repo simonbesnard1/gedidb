36c82bee25a6913c7b0df8a9ad078363521939eb
Simon <besnard@gfz-potsdam.de>
2024-09-04 16:48:30 +0200

    add nasa api shutdown handler for downloading

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -1,6 +1,4 @@
 from gedidb.core.gediprocessor import GEDIGranuleProcessor
-from gedidb.database.db import DatabaseManager
-from gedidb.providers.db_provider import DatabaseInfoRetriever
 
 #%% Initiate database builder
 database_builder = GEDIGranuleProcessor(data_config_file = "./config_files/data_config.yml", sql_config_file='./config_files/db_scheme.sql')
