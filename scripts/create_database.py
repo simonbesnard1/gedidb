5b6c0e154d5b223fb0dfc10640b30943dbd24872
felixd <felixd@gfz-potsdam.de>
2024-09-05 15:16:15 +0200

    added config support to database tables

--- scripts/create_database.py
+++ scripts/create_database.py
@@ -1,7 +1,7 @@
 from gedidb.core.gediprocessor import GEDIGranuleProcessor
 
 #%% Initiate database builder
-database_builder = GEDIGranuleProcessor(data_config_file = "./config_files/data_config.yml", sql_config_file='../config_files/db_scheme.sql')
+database_builder = GEDIGranuleProcessor(data_config_file = "../config_files/data_config.yml", sql_config_file='../config_files/db_scheme.sql')
 
 #%% Process GEDI data
 database_builder.compute()
