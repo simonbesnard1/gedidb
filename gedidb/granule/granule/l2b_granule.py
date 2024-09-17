e4a76cc5c8b94b602396372abde6648a140061a5
Simon <besnard@gfz-potsdam.de>
2024-09-17 16:34:22 +0200

    add license to python files

--- gedidb/granule/granule/l2b_granule.py
+++ gedidb/granule/granule/l2b_granule.py
@@ -1,3 +1,12 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Version: 2.0
+# Contact: ah2174@cam.ac.uk, felix.dombrowski@uni-potsdam.de and besnard@gfz-potsdam.de
+# SPDX-FileCopyrightText: 2024 Simon Besnard
+# SPDX-FileCopyrightText: 2024 Felix Dombrowski
+# SPDX-FileCopyrightText: 2024 Amelia Holcomb
+# SPDX-FileCopyrightText: 2024 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
+#
+
 from gedidb.granule.granule.granule import Granule
 from gedidb.granule.beam.beam import Beam
 from gedidb.granule.beam.l2b_beam import L2BBeam
--- gedidb/tests/test_nasa_cmr_api.py
+++ gedidb/tests/test_nasa_cmr_api.py
@@ -1,3 +1,12 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Version: 2.0
+# Contact: ah2174@cam.ac.uk, felix.dombrowski@uni-potsdam.de and besnard@gfz-potsdam.de
+# SPDX-FileCopyrightText: 2024 Simon Besnard
+# SPDX-FileCopyrightText: 2024 Felix Dombrowski
+# SPDX-FileCopyrightText: 2024 Amelia Holcomb
+# SPDX-FileCopyrightText: 2024 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
+#
+
 """Test that the NASA CMR API has not changed recently.
 This test will probably be flaky because it depends on the NASA CMR API.
 However, if we don't run this test, they tend to silently disable old API 
