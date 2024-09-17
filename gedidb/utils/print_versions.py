4035be3dcb40eb50a3525688d54a2e39e4426a63
Simon <besnard@gfz-potsdam.de>
2024-09-17 16:34:22 +0200

    add license to python files

--- gedidb/utils/print_versions.py
+++ gedidb/utils/print_versions.py
@@ -1,3 +1,12 @@
+# SPDX-License-Identifier: EUPL-1.2
+# Version: 2.0
+# Contact: ah2174@cam.ac.uk, felix.dombrowski@uni-potsdam.de and besnard@gfz-potsdam.de
+# SPDX-FileCopyrightText: 2024 Simon Besnard
+# SPDX-FileCopyrightText: 2024 Felix Dombrowski
+# SPDX-FileCopyrightText: 2024 Amelia Holcomb
+# SPDX-FileCopyrightText: 2024 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
+#
+
 """Utility functions for printing version information."""
 
 import importlib
