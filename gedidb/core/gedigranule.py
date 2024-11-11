879b7910e76788997dd9ddf9fcc16c78af7c3b75
Simon <besnard@gfz-potsdam.de>
2024-11-11 19:31:19 +0100

    move delete dir to parse_granules

--- gedidb/core/gedigranule.py
+++ gedidb/core/gedigranule.py
@@ -118,13 +118,6 @@ class GEDIGranule:
         self.data_writer.write_granule(gdf)
         self.data_writer.mark_granule_as_processed(granule_key)
         
-        granule_dir = os.path.join(self.download_path, granule_key)
-        
-        try:
-            shutil.rmtree(granule_dir)
-        except Exception as e:
-            logger.error(f"Error deleting directory {granule_dir}: {e}")
-
 
     def parse_granules(self, granules: List[Tuple[str, str]], granule_key: str) -> Dict[str, Dict[str, np.ndarray]]:
         """
@@ -136,7 +129,8 @@ class GEDIGranule:
             Dictionary of dictionaries, each containing NumPy arrays for each product.
         """
         data_dict = {}
-    
+        granule_dir = os.path.join(self.download_path, granule_key)
+        
         for product, file in granules:
             data = granule_parser.parse_h5_file(file, product, data_info=self.data_info)
             if data is not None:
@@ -144,6 +138,8 @@ class GEDIGranule:
             else:
                 logger.warning(f"Skipping product {product} for granule {granule_key} due to parsing failure.")
         
+        shutil.rmtree(granule_dir)
+                    
         return {k: v for k, v in data_dict.items() if "shot_number" in v}
     
     @staticmethod
