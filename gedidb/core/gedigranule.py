0f9df0e93433b2417a7d97fb566fb98a841dd5a9
Simon <besnard@gfz-potsdam.de>
2025-01-17 13:09:53 +0100

    update license header

--- gedidb/core/gedigranule.py
+++ gedidb/core/gedigranule.py
@@ -1,10 +1,10 @@
 # SPDX-License-Identifier: EUPL-1.2
 # Version: 2.0
-# Contact: ah2174@cam.ac.uk, felix.dombrowski@uni-potsdam.de and besnard@gfz-potsdam.de
-# SPDX-FileCopyrightText: 2024 Simon Besnard
-# SPDX-FileCopyrightText: 2024 Felix Dombrowski
-# SPDX-FileCopyrightText: 2024 Amelia Holcomb
-# SPDX-FileCopyrightText: 2024 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
+# Contact: besnard@gfz.de, felix.dombrowski@uni-potsdam.de and ah2174@cam.ac.uk
+# SPDX-FileCopyrightText: 2025 Amelia Holcomb
+# SPDX-FileCopyrightText: 2025 Felix Dombrowski
+# SPDX-FileCopyrightText: 2025 Simon Besnard
+# SPDX-FileCopyrightText: 2025 Helmholtz Centre Potsdam - GFZ German Research Centre for Geosciences
 #
 
 import os
