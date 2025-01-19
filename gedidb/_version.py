8bd891231e6fe2f97cc3d63367d05a299d8e2ffa
Simon <besnard@gfz-potsdam.de>
2025-01-19 15:50:00 +0100

    fix versioning

--- gedidb/_version.py
+++ gedidb/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.post4'
-__version_tuple__ = version_tuple = (0,)
+__version__ = version = '2025.2.1.dev0+g822becd.d20250119'
+__version_tuple__ = version_tuple = (2025, 2, 1, 'dev0', 'g822becd.d20250119')
